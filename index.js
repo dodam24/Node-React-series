466d9b125dda6aa8c536ac0dfefc70905f19388d
dodam24 <dodam0724@gmail.com>
2023-09-25 23:46:15 +0900

    Node JS - 토큰 생성 with jsonwebtoken

--- index.js
+++ index.js
@@ -25,17 +25,59 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/register', (req, res) => {
 
   // 회원가입 할 때 필요한 정보들을 client에서 가져오면
-  // 그 정보들을 데이터 베이스에 넣어준다.
+  // 그 정보들을 데이터베이스에 넣어준다.
   const user = new User(req.body)
 
+
+  // Mongoose 버전 업데이트에 따라 model.save()에 콜백 함수 전달 X
+  /*
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err })
     return res.status(200).json({
       success: true
     })
   })
+  */
+  // Mongoose 버전 업데이트에 따른 에러 발생으로, 기존 코드 대신 Promise를 사용하여 save()를 호출
+  user.save()
+    .then(userInfo => {
+      return res.status(200).json({
+        success: true
+      })
+    })
+    .catch(err => {
+      return res.json({ success: false, err })
+    });
 })
 
+app.post('/login', (req, res) => {
 
+  // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "이메일에 해당하는 유저가 없습니다."
+      })
+    }
+  })
+
+  // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+  user.comparePassword(req.body.password, (err, isMatch) => {
+    if(!isMatch)
+      return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+    
+
+    // 비밀번호까지 맞다면 토큰을 생성하기.
+    user.generateToken((err, user) => {
+      
+    })
+      
+
+
+  })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
